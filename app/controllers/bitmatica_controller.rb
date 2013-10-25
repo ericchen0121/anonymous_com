e217997a7192d9a23e3fac59ed0daf73ba3b9b8a
Josh Miramant <josh@miramant.me>
2013-10-25 09:10:20 -0700

    half of rough html outline. DO NOT MERGE WITHOUT TALKING WITH ME

--- app/controllers/bitmatica_controller.rb
+++ app/controllers/bitmatica_controller.rb
@@ -1,8 +1,15 @@
 class BitmaticaController < ActionController::Base
   protect_from_forgery
   layout "application" 
-
   def index
+  	@services = [
+  						{path: "PRODUCT_DESIGN_ICON", title: "Production Design & Consulting", copy: "Combining customer validayion, competitive analysis, and value creation. Our first priority is to make your product succeed." },
+  						{path: "VISUAL_DESIGN_ICON", title: "Visual Interface & Experience Design", copy: "We create stunning visual and interactive experiences that keep your customers engaged from the first impression."},
+  						{path: "SECURITY_ICON", title: "Security Consulting", copy: "Our extensive background in security and crytography helps you find and fix vulernabilites before attackers do."},
+  						{path: "BUSINESS_INTEL_ICON", title: "Business Intellegence, Scaling & Operations", copy: "From one to one billion users, Bitmatica can give you the cusomer insight you need to grow."},
+  						{path: "WEB_APP_ICON", title: "Web Application Development", copy: "Ruby, Python, JavaScript, SQL and more - we speak it all to deliver the most robust, efficiant and maintainable applications for your bussiness."},
+  						{path: "WEB_APP_ICON", title: "Mobile Application Development", copy: "As the world goes more mobile, leverage our native iOS and Android expertise to open new markets and evolve your customer base."},
+						  	]
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+	def row_fluid(html_array, nb_cols = 3)
+    result = '<div class="row-fluid">'
+
+    html_array.each_with_index do |html, index|
+      if index % nb_cols === 0
+        if index === 0
+          result += '<div class="row-fluid">'
+          result += html
+        else
+          result += '</div>'
+          result += '<div class="row-fluid">'
+          result += html
+        end
+      else
+        result += html
+      end
+    end
+
+    result += '</div></div>'
+    result.html_safe
+  end
+
 end
